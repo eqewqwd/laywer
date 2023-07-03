636687146c5c3e53d30e904961f12a7fe9586cac
david-d1512 <daviddabah3@gmail.com>
2023-07-04 00:01:13 +0300

    teset2

---
+++ functions/PostDataCollectionCart.js
@@ -0,0 +1,30 @@
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://admin:davidbenzohar3@sweetcake.gadahcn.mongodb.net/?retryWrites=true&w=majority";
+
+
+exports.handler = async function(event, context) {
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  await client.connect();
+
+  const db = client.db("CakesDB");
+  const collection = db.collection("CartList");
+
+  const data = JSON.parse(event.body);
+
+  try {
+    const result = await collection.insertOne(data);
+    console.log('Data saved to MongoDB:', result);
+    return {
+      statusCode: 200,
+      body: 'Form submitted successfully'
+    };
+  } catch (error) {
+    console.error('Error saving data to MongoDB:', error);
+    return {
+      statusCode: 500,
+      body: 'Error submitting form'
+    };
+  } finally {
+    await client.close();
+  }
+};
