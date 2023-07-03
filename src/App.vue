dea9afdc6124e1a1d71be0f644ad6d989421d359
david-d1512 <daviddabah3@gmail.com>
2023-07-03 23:47:10 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+    <Calendar/>
+</template>
+
+<script>
+import Calendar from './components/Calendar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Calendar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
---
+++ src/components/Calendar.vue
@@ -0,0 +1,58 @@
+<template>
+  <FullCalendar 
+  :options='calendarOptions'
+  :header="{
+    left:'title',
+    center:'dayGridMonth,timeGridWeek, timeGridDay, listWeek',
+    right:'prev today next'
+  }"
+  :buttonText="{
+    today:'היום',
+    month:'חודש',
+    week:'שבוע',
+    day:'יום',
+    list:'רשימה',
+  }"
+  />
+</template>
+
+<script>
+
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import listWeek from '@fullcalendar/list'
+
+export default {
+  name: 'Calendar',
+  data: function() {
+    return {
+      calendarOptions: {
+        locale:"he",
+        plugins: [listWeek],
+        initialView: 'listWeek',
+        headerToolbar: {
+          left:'title',
+          center:'dayGridMonth,timeGridWeek, timeGridDay, listWeek',
+          right:'prev today next'
+        },
+        events: [
+        { title: 'Meeting', start: Date.parse('07 02 2023 22:27'), end:Date.parse('08 02 2023 22:27')},
+        { title: 'Meeting', start: Date.parse('07 02 2023 22:27'), end:Date.parse('08 02 2023 22:27'),},
+        { title: 'Meeting', start: Date.parse('07 02 2023 22:27'), end:Date.parse('08 02 2023 22:27'), color:'red'}
+        ]
+      }
+    }
+  },
+  components:{FullCalendar},
+  mounted(){
+    console.log(new Date())
+  },
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
