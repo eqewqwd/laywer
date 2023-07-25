bc5b87d04ac54d5ea95c3b21125444a2444ed1d4
david-d1512 <daviddabah3@gmail.com>
2023-07-25 23:19:30 +0300

    lk

--- functions/user-data.js
+++ functions/user-data.js
@@ -1,7 +1,7 @@
 // netlify-functions/user/user.js
 
 const mongoose = require('mongoose');
-const verifyToken = require('/.netlify/functions/user-data');
+const verifyToken = require('verify');
 
 const userSchema = new mongoose.Schema({
   username: String,
