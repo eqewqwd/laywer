ada5565ad5ce1df589f3ad1bd2af7ff0140038d2
david-d1512 <daviddabah3@gmail.com>
2023-07-28 18:03:13 +0300

    davd

--- functions/update-Item.js
+++ functions/update-Item.js
@@ -4,32 +4,34 @@ const MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectId;
 
 exports.handler = async (event) => {
-  try {
-    const { id, updatedData } = JSON.parse(event.body);
-    var newId = ObjectId(id)
+  console.log(event)
+  console.log(event.body)
+  // try {
+  //   const { id, updatedData } = JSON.parse(event.body);
+  //   var newId = ObjectId(id)
 
-    const uri = "mongodb+srv://aviadbenzohar5:ZNpcQIHRxUfTORmx@cluster0.frsyu1a.mongodb.net/?retryWrites=true&w=majority";
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    await client.connect();
+  //   const uri = "mongodb+srv://aviadbenzohar5:ZNpcQIHRxUfTORmx@cluster0.frsyu1a.mongodb.net/?retryWrites=true&w=majority";
+  //   const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  //   await client.connect();
 
-    const db = client.db('lawyerWeb');
-    const collection = db.collection('HomePage');
+  //   const db = client.db('lawyerWeb');
+  //   const collection = db.collection('HomePage');
 
-    // Update the item in MongoDB
-    const result = await collection.updateOne({ _id: newId  }, { $set: updatedData });
+  //   // Update the item in MongoDB
+  //   const result = await collection.updateOne({ _id: newId  }, { $set: updatedData });
 
-    client.close();
+  //   client.close();
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: 'Item updated successfully' }),
-    };
-  } catch (error) {
-    console.error('Error:', error);
+  //   return {
+  //     statusCode: 200,
+  //     body: JSON.stringify({ message: 'Item updated successfully' }),
+  //   };
+  // } catch (error) {
+  //   console.error('Error:', error);
 
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ message: 'An error occurred while updating the item' }),
-    };
-  }
+  //   return {
+  //     statusCode: 500,
+  //     body: JSON.stringify({ message: 'An error occurred while updating the item' }),
+  //   };
+  // }
 };
