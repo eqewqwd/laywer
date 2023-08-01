83c776ed520ccf51f6120682c80ca08f27f22705
david-d1512 <daviddabah3@gmail.com>
2023-08-02 00:19:30 +0300

    res

--- functions/uploadPhoto.js
+++ functions/uploadPhoto.js
@@ -34,8 +34,17 @@ exports.handler = async function (event, context) {
     if (event.httpMethod === "GET") {
       const photos = await collection.find().toArray();
       const photoList = photos.map((photo) => {
+        var fixUrl
+        if(photo.type == 'image/png'){
+            fixUrl = "data:image/png;base64,"
+        }else if(photo.type == 'image/jpg'){
+            fixUrl = "data:image/jpg;base64,"
+        }else if(photo.type == 'image/jpeg'){
+            fixUrl = "data:image/jpeg;base64,"
+        }
         return {
-          image:`data:${photo.type};base64,` + photo.image.toString("base64"),
+            
+          image:fixUrl + photo.image.toString("base64"),
         };
       });
       return {
