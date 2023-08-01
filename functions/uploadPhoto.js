e4d09bf2cc47a56208aa5d478f8cb9cac33737bd
david-d1512 <daviddabah3@gmail.com>
2023-08-01 20:12:03 +0300

    ujjju

--- functions/uploadPhoto.js
+++ functions/uploadPhoto.js
@@ -1,37 +1,29 @@
 // netlify-functions/uploadPhoto.js
-const MongoClient = require('mongodb').MongoClient;
+const { MongoClient } = require('mongodb');
 const uri = "mongodb+srv://aviadbenzohar5:ZNpcQIHRxUfTORmx@cluster0.frsyu1a.mongodb.net/?retryWrites=true&w=majority";
 
-exports.handler = async (event, context) =>{
-    const imageData = JSON.parse(event.body).image;
-    return {
-        statusCode: 200,
-        body: JSON.stringify(imageData),
-    };
-    try {
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+exports.handler = async (event, context) => {
+  try {
+    const client = new MongoClient(uri);
     await client.connect();
 
-    const db = client.db('lawyerWeb');
-    const collection = db.collection('images');
+    const db = client.db("lawyerWeb");
+    const collection = db.collection("images");
 
     // Parse the incoming base64 image data
     const imageData = JSON.parse(event.body).image;
-    return {
-        statusCode: 200,
-        body: JSON.stringify(imageData),
-    };
     const buffer = Buffer.from(imageData, 'base64');
-   
+
     // Insert the image into MongoDB
     const result = await collection.insertOne({ image: buffer });
 
     client.close();
 
-    // return {
-    //   statusCode: 200,
-    //   body: JSON.stringify({ message: 'Photo uploaded successfully' }),
-    // };
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Photo uploaded successfully' }),
+    };
   } catch (error) {
     return {
       statusCode: 500,
