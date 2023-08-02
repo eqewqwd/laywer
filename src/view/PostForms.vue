4e3bc6c6fa36d50cf49cdbb4dfba5f8d965e2345
david-d1512 <daviddabah3@gmail.com>
2023-08-02 10:50:13 +0300

    ewq

--- src/view/PostForms.vue
+++ src/view/PostForms.vue
@@ -76,17 +76,16 @@ export default {
         const reader = new FileReader();
         reader.readAsDataURL(this.selectedFile);
         reader.onload = async () => {
-          const base64String = null;
           if(base64String == "data:image/png;base64,"){
-            base64String = reader.result.replace("data:image/png;base64,", "");
+            var base64String = reader.result.replace("data:image/png;base64,", "");
           }else if(reader.result == "data:image/jpg;base64,"){
-            base64String = reader.result.replace("data:image/jpg;base64,", "");
+            var base64String = reader.result.replace("data:image/jpg;base64,", "");
           }else if(reader.result == "data:image/jpeg;base64,"){
-            base64String = reader.result.replace("data:image/jpeg;base64,", "");
+            var base64String = reader.result.replace("data:image/jpeg;base64,", "");
           }else if(reader.result == "data:image/JPG;base64,"){
-            base64String = reader.result.replace("data:image/JPG;base64,", "");
+            var base64String = reader.result.replace("data:image/JPG;base64,", "");
           }else if(reader.result == "data:image/JPEG;base64,"){
-            base64String = reader.result.replace("data:image/JPEG;base64,", "");
+            var base64String = reader.result.replace("data:image/JPEG;base64,", "");
           }
           const typeProp = this.selectedFile.type
           console.log(base64String)
