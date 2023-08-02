b6d1b1ae8c9496828f6b78d638b70ef7852b50d6
david-d1512 <daviddabah3@gmail.com>
2023-08-02 10:23:54 +0300

    res

--- src/view/PostForms.vue
+++ src/view/PostForms.vue
@@ -76,7 +76,13 @@ export default {
         const reader = new FileReader();
         reader.readAsDataURL(this.selectedFile);
         reader.onload = async () => {
-          const base64String = reader.result.replace("data:image/jpeg;base64,", "");
+          if(reader.result == "data:image/png;base64,"){
+            var base64String = reader.result.replace("data:image/png;base64,", "");
+          }else if(reader.result == "data:image/jpg;base64,"){
+            var base64String = reader.result.replace("data:image/jpg;base64,", "");
+          }else if(reader.result == "data:image/jpeg;base64,"){
+            var base64String = reader.result.replace("data:image/jpeg;base64,", "");
+          }
           const typeProp = this.selectedFile.type
           const response = await axios.post(
             "/.netlify/functions/uploadPhoto",
